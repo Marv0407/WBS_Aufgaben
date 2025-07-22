6850ccfefe7452ea232dc5a8bbe7351f7076af48
Marvin <141830291+Marv0407@users.noreply.github.com>
2025-07-22 23:30:34 +0200

    Gui

--- src/RestaurantBestellsystem/RestaurantApp.java
+++ src/RestaurantBestellsystem/RestaurantApp.java
@@ -4,12 +4,11 @@ import RestaurantBestellsystem.model.MenuItem;
 import RestaurantBestellsystem.model.Order;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class RestaurantApp {
     public static ArrayList<MenuItem> menu = new ArrayList<>();
-
-
     public static void addMenuItem(MenuItem menuItem) {
         menu.add(menuItem);
     }
@@ -33,12 +32,22 @@ public class RestaurantApp {
         addMenuItem(new MenuItem("Pizza Diavolo", 11.00));
         addMenuItem(new MenuItem("Lasagne", 17.00));
 
+
+
+
+
+
+
+
+
         System.out.println("===== WELCOME TO JAVA RESTAURANT =====\nUnsere Speisekarte:\n");
         for(MenuItem i: menu){
             System.out.println(i);
         }
         Order order = new Order("Jeffrey Epstein");
+
         order.getOrder();
+
 
 
         order.printQuittung();
